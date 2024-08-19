a4a2e2be4a4bbe07849ca27ed88f338d12566416
Anish <dholliwar77@gmail.com>
2024-08-19 14:00:43 -0700

    Updating Images

--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,12 @@ import React from 'react';
 import { FaReact, FaSalesforce, FaTiktok } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
 import anish from '@/public/anish.png';
+import rogue from '@/public/Rogue.jpg';
+import webscrape from '@/public/webscrape.jpg';
+import lichess from '@/public/lichess.jpg';
+import asteroid from '@/public/asteroids.jpg';
+import java from '@/public/java.jpg';
+import aws from '@/public/aws.jpg';
 
 export const links = [
   {
@@ -79,41 +85,41 @@ export const projectsData = [
     description:
       'Created a Django website for Rogue Media to give them data analytics on their users using Python and Django.',
     tags: ['Python', 'Django'],
-    imageUrl: anish,
+    imageUrl: rogue,
   },
   {
     title: 'Price Comparison Web Scraper',
     description:
       'Created a Python web scraper that allows users to search for an item across the internet. This sorts the data according to the lowest price or alphabetically for the user which is displayed on my flask website.',
-    imageUrl: anish,
+    imageUrl: webscrape,
   },
   {
     title: 'Chess Opening Move Search Engine',
     description:
       'Created a Python chess search engine that allows users to search various openings from a real Lichess database using search engine optimization practices. Integrated with a GUI using PyQt5',
     tags: ['Python', 'Qt'],
-    imageUrl: anish,
+    imageUrl: lichess,
   },
   {
     title: '2D Asteroid Mobile Game',
     description:
       'Created an original take on the classic Asteroid game using Unity 2D and C# scripts. Includes multiple enemies AI with patterns, art assets, and sound assets.',
     tags: ['C#, Unity'],
-    imageUrl: anish,
+    imageUrl: asteroid,
   },
   {
     title: 'Movie Rating Application',
     description:
       'Created a movie rating application in which users can log in and rate various movies and keep a sorted list of movies.',
     tags: ['Java', 'JavaFX', 'XAMPP', 'Scene Builder'],
-    imageUrl: anish,
+    imageUrl: java,
   },
   {
     title: 'AWS Facial Recognition ',
     description:
       'Created a facial recognition system, using python and a raspberry pi attached to a camera to produce a high confidence interval to recognize faces',
     tags: ['Python, AWS'],
-    imageUrl: anish,
+    imageUrl: aws,
   },
 
 ] as const;
